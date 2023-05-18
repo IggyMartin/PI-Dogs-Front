1084a7279ecf3ee49530f31fe1c60f86a929834d
Ignacio Martin <ignaciomartin@Ignacios-MacBook-Pro.local>
2023-05-18 18:02:20 -0300

    front redeploy try

--- src/index.js
+++ src/index.js
@@ -7,8 +7,8 @@ import { Provider } from 'react-redux'
 import store from './redux/store.js'
 import axios from 'axios'
 
-//axios.defaults.baseURL = 'http://localhost:3001'
-axios.defaults.baseURL = 'https://pi-dogs-back-production-f4e6.up.railway.app/'
+/* axios.defaults.baseURL = 'http://localhost:3001' */
+axios.defaults.baseURL = 'https://pi-dogs-back-production-f4e7.up.railway.app'
 
 ReactDOM.render(
   <Provider store={store}>
